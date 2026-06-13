e71e2ef3858722532a30d5fbf06fc7c4069fcaf4
Rutuja Karande <rutujakarande967@gmail.com>
2026-06-13 09:58:15 +0530

    new file

---
+++ src/Day_01/BasicExample1.java
@@ -0,0 +1,7 @@
+package Day_01;
+
+public class BasicExample1 {
+    public static void main(String[]args){
+        System.out.println("Hello Java");
+    }
+}
---
+++ src/Day_01/BasicExample2.java
@@ -0,0 +1,16 @@
+package Day_01;
+
+public class BasicExample2 {
+    public static void main(String[]args){
+        String name = "Ajinkya Jadhav";
+        int age = 25;
+        double salary = 50000.50;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        System.out.println("name:" + name);
+        System.out.println("age:" + age);
+        System.out.println("salary:" + salary);
+        System.out.println("grade:" + grade);
+        System.out.println(isJavaFun);
+    }
+}
