d969043167e1994b5e9b6e8bb47f4be24bf1b8e3
Rutuja Karande <rutujakarande967@gmail.com>
2026-06-13 10:10:09 +0530

    new file

---
+++ src/Day_01/BasicExample3.java
@@ -0,0 +1,13 @@
+package Day_01;
+
+public class BasicExample3 {
+    public static void main(String[]args){
+        String name = "Rutuja Karande";
+        int age = 20;
+        String city = "Tuljapur";
+
+        System.out.println("Name:"+ name);
+        System.out.println("Age:"+ age);
+        System.out.println("City:"+ city);
+    }
+}
---
+++ src/Day_01/CalculateSimpleInterest.java
@@ -0,0 +1,12 @@
+package Day_01;
+
+public class CalculateSimpleInterest {
+    public static void main(String[]args){
+         int p = 1000;
+         int r = 5;
+         int t = 2;
+         int calculate = (p*r*t)/100;
+         System.out.println("calculate simple interest:" + calculate);
+
+    }
+}
